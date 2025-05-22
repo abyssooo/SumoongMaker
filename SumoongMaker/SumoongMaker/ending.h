cf363c4de9b4d5c3d1819f0aec8fb3c65a359432
kaizzang64 <kaizzang64@gmail.com>
2025-05-22 13:42:05 +0900

    ending.h

---
+++ SumoongMaker/SumoongMaker/ending.h
@@ -0,0 +1,26 @@
+#define ENDING_H
+
+#include <string>
+#include "Stat.h"
+
+using namespace std;
+
+class Ending {
+public:
+    // 생성자: 게임이 끝난 후 최종 Stat을 입력받음
+    Ending(const Stat& finalStat);
+
+    // 엔딩 이름을 반환하는 함수
+    string getEnding() const;
+
+private:
+    Stat playerStat; // 플레이어의 최종 스탯
+
+    // 각 엔딩을 판단하는 함수들
+    bool isBadEnding() const;
+    bool isHappyEnding() const;
+    bool isCharmEnding() const;
+    bool isStrengthEnding() const;
+    bool isArtEnding() const;
+    bool isIntelligenceEnding() const;
+};
