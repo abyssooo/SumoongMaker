514b74fc710866085183f884fbd01653c6015592
kaizzang64 <kaizzang64@gmail.com>
2025-05-31 15:00:35 +0900

    Update Character.cpp

--- SumoongMaker/SumoongMaker/Character.cpp
+++ SumoongMaker/SumoongMaker/Character.cpp
@@ -1,5 +1,25 @@
 #include "Character.h"
 #include <iostream>
+#include <iomanip>
+#include <algorithm> // max
+
+using namespace std;
+
+static const char* ascii_art[] = {
+    "        /\\   ♂ ♀   /\\        ",
+    "       /  \\‾‾‾‾‾‾‾/  \\       ",
+    "      /   ( ^ w ^ )   \\      ",
+    "     |     ( ‿ )       |     ",
+    "     |   \\_______/     |     ",
+    "    /|     _____       |\\    ",
+    "   / |    |     |      | \\   ",
+    "  |  |    | [_] |      |  |  ",
+    "  |  |    |_____|    __|  |  ",
+    "  |  |   /       \\  /  |  |  ",
+    "   \\_|__|         |____|_/   ",
+    "      (__)       (__)        ",
+    "      /  \\       /  \\        "
+};
 
 Character::Character(string name) : name(name) {}
 
@@ -8,12 +28,37 @@ void Character::applyAct(const Act& act) {
     cout << act.getName() << " 행동을 수행했습니다." << endl;
 }
 
-void Character::showStats() const {
-    cout << "현재 능력치:" << endl;
-    cout << "매력: " << stats.getCharm() << endl;
-    cout << "지능: " << stats.getIntelligence() << endl;
-    cout << "체력: " << stats.getStrength() << endl;
-    cout << "예술: " << stats.getArt() << endl;
-    cout << "스트레스: " << stats.getStress() << endl;
-    cout << "돈: " << stats.getMoney() << endl;
+// 통합: 화면 클리어 + 좌측 스탯 + 우측 아트
+void Character::showStatsWithAsciiArt() const {
+    cout << "\033[1;1H";  // 커서 이동
+    cout << "\033[2J";    // 화면 지우기
+
+    const int statColWidth = 30;
+    const int artLines = sizeof(ascii_art) / sizeof(ascii_art[0]);
+
+    string statText[] = {
+        "[현재 능력치]",
+        "매력     : " + to_string(stats.getCharm()),
+        "지능     : " + to_string(stats.getIntelligence()),
+        "체력     : " + to_string(stats.getStrength()),
+        "예술     : " + to_string(stats.getArt()),
+        "스트레스 : " + to_string(stats.getStress()),
+        "돈       : " + to_string(stats.getMoney()),
+        "------------------------"
+    };
+
+    int statLineCount = sizeof(statText) / sizeof(statText[0]);
+    int maxLines = max(statLineCount, artLines);
+
+    for (int i = 0; i < maxLines; ++i) {
+        if (i < statLineCount)
+            cout << left << setw(statColWidth) << statText[i];
+        else
+            cout << setw(statColWidth) << " ";
+
+        if (i < artLines)
+            cout << ascii_art[i];
+
+        cout << endl;
+    }
 }
